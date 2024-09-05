fdd927af3ae559b45bb0a4e6b0d36777a1e03ac3
suzannedungo <2022310116@dhvsu.edu.ph>
2024-09-05 14:59:54 +0800

    Final

--- dashboard/admin/index.php
+++ dashboard/admin/index.php
@@ -17,9 +17,23 @@ $user_data = $stmt->fetch(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ADMIN DASHBOARD</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../src/css/style.css">
 </head>
 <body>
-    <h1>WELCOME <?php echo $user_data['email']?></h1>
-    <button><a href="authentication/admin-class.php?admin_signout">SIGN OUT</a></button>
+    <div class="container">
+    <h1 class="welcome"> WELCOME <?php echo $user_data['email']?></h1>
+    
+    <div class="fg">
+        <button><a href="authentication/admin-class.php?admin_signout" class="bp">SIGN OUT</a></button>
+    </div>
+
+    <div class="jmc">
+        <img src="../../src/img/jm.png" alt="jmc"width=250px height= 200px>
+    </div>
+
+        <p class="xmas"> Whenever I see...</p>
+    </div>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -7,24 +7,53 @@ include_once 'config/settings-configuration.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="src/css/style.css">
 </head>
 <body>
+    <div class="container">
     <h1>SIGN IN</h1>
-    <form action="dashboard/admin/authentication/admin-class.php" method="POST">
-        <input type="hidden" name="csrf_token" value="<?php  echo  $csrf_token?>">
-        <input type="email" name="email" placeholder="Enter Email" required><br>
-        <input type="password" name="password" placeholder="Enter Password" required><br>
-        <button type="submit" name="btn-signin">SIGN IN</button>
+     <form action="dashboard/admin/authentication/admin-class.php" method="POST">
+        <div class="form-group">
+            <input type="hidden" class="form-control" name="csrf_token" value="<?php  echo  $csrf_token?>">
+        </div>
+
+        <div class="form-group">
+            <input type="email" class="form-control" name="email" placeholder="Enter Email" required>
+        </div>
+
+        <div class="form-group">
+            <input type="password" class="form-control" name="password" placeholder="Enter Password" required>
+        </div>
+
+        <div class="form-group">
+            <button type="submit" class="btn-primary" name="btn-signin">SIGN IN</button>
+        </div>
     </form>
 
     <h1>REGISTRATION</h1>
     <form action="dashboard/admin/authentication/admin-class.php" method="POST">
-        <input type="hidden" name="csrf_token" value="<?php  echo  $csrf_token?>">
-        <input type="text" name="username" placeholder="Enter Username"required><br>
-        <input type="email" name="email" placeholder="Enter Email" required><br>
-        <input type="password" name="password" placeholder="Enter Password" required><br>
-        <button type="submit" name="btn-signup">SIGN UP</button>
+        <div class="form-group">
+            <input type="hidden" class="form-control" name="csrf_token" value="<?php  echo  $csrf_token?>">
+        </div>
+
+        <div class="form-group">
+            <input type="text" class="form-control" name="username" placeholder="Enter Username"required>
+        </div>
+
+        <div class="form-group">
+            <input type="email" class="form-control" name="email" placeholder="Enter Email" required>
+        </div>
+
+        <div class="form-group">
+            <input type="password" class="form-control" name="password" placeholder="Enter Password" required>
+        </div>
+
+        <div class="form-group">
+            <button type="submit" class="btn-primary" name="btn-signup">SIGN UP</button>
+        <div class="form-group">
     </form>
+    </div>
 </body>
 </html>
